6e56e4582b75f749b1a209d9fb386dd5f17c6543
Damngz <da.munozgz@gmail.com>
2025-06-22 17:50:31 -0400

    base project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AuthGuard } from "./auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./login/login.component').then(load => load.LoginComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(load => load.LoginComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.component').then(load => load.HomeComponent)
+  }
+]
